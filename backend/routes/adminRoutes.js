28c8111e872519e03555b41ccba65c76bcc763da
AMAL <mailtoamaldevcd@gmail.com>
2023-07-31 21:19:16 +0530

    changed the middleware for report

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -7,6 +7,7 @@ const { suspend } = require('../controllers/admin/suspend')
 const { login } = require('../controllers/admin/login')
 const { verifyAdmin } = require('../middlewares/adminAuth')
 const {reportPost} = require('../controllers/admin/reportPost')
+const { verifyToken } = require('../middlewares/userAuth')
 
 //Base URL: /api/admin
 router.patch('/approveCompany/:id',verifyAdmin,approveCompany)
@@ -14,6 +15,6 @@ router.get('/pendingCompanyApproval',verifyAdmin,pendingCompanyApproval)
 router.get('/reported/:content',verifyAdmin,reported)
 router.patch('/suspend/:content/:id',verifyAdmin,suspend)
 router.post('/login',login)
-router.put('/report/:content/:id',verifyAdmin,reportPost);
+router.put('/report/:content/:id',verifyToken,reportPost);
 
 module.exports = router
