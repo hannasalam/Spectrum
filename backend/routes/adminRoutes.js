efb1e91329fd5b5bf4fdbdce0f03034a464ef023
AMAL <mailtoamaldevcd@gmail.com>
2023-07-31 08:39:35 +0530

    report functionality added

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -6,6 +6,7 @@ const { reported } = require('../controllers/admin/reported')
 const { suspend } = require('../controllers/admin/suspend')
 const { login } = require('../controllers/admin/login')
 const { verifyAdmin } = require('../middlewares/adminAuth')
+const {reportPost} = require('../controllers/admin/reportPost')
 
 //Base URL: /api/admin
 router.patch('/approveCompany/:id',verifyAdmin,approveCompany)
@@ -13,5 +14,6 @@ router.get('/pendingCompanyApproval',verifyAdmin,pendingCompanyApproval)
 router.get('/reported/:content',verifyAdmin,reported)
 router.patch('/suspend/:content/:id',verifyAdmin,suspend)
 router.post('/login',login)
+router.put('/report/:content/:id',verifyAdmin,reportPost);
 
 module.exports = router
